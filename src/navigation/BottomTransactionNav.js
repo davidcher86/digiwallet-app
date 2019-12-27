15ebf5818acf576664c8d1bab394742616d6fd30
Davidcher86 <davidcher86@gmail.com>
2019-12-27 17:23:23 +0300

    - more bottom drawer style

--- src/navigation/BottomTransactionNav.js
+++ src/navigation/BottomTransactionNav.js
@@ -20,18 +20,26 @@ class BottomTransactionsStack extends Component {
 
   render() {
     const {navigate} = this.props.navigation;
+
+    const ifSelected = (key) => {
+      const index = this.props.navigation.state.index;
+      const keyName = this.props.navigation.state.routes[index].key
+      
+      return keyName === key;
+    }
+
     return (
       <View style={styles.container}>
-        <View style={styles.tabContainer}>
+        <View style={[styles.tabContainer, (ifSelected('Daily') ? styles.selectedTab : '')]}>
           <TouchableOpacity
             onPress={() => navigate('Daily')}
             style={styles.containerBottomItem}>
             <View style={styles.button}>
-              <Text>daily</Text>
+              <Text>Daily</Text>
             </View>
           </TouchableOpacity>
         </View>
-        <View style={styles.tabContainer}>
+        <View style={[styles.tabContainer, (ifSelected('Weekly') ? styles.selectedTab : '')]}>
           <TouchableOpacity
             onPress={() => navigate('Weekly')}
             style={styles.containerBottomItem}>
@@ -40,21 +48,30 @@ class BottomTransactionsStack extends Component {
             </View>
           </TouchableOpacity>
         </View>
-        <View style={styles.tabContainer}>
+        <View style={[styles.tabContainer, (ifSelected('Monthly') ? styles.selectedTab : '')]}>
           <TouchableOpacity
-            onPress={() => navigate('monthly')}
+            onPress={() => navigate('Monthly')}
             style={styles.containerBottomItem}>
             <View style={styles.button}>
-              <Text>monthly</Text>
+              <Text>Monthly</Text>
             </View>
           </TouchableOpacity>
         </View>
-        <View style={styles.tabContainer}>
+        <View style={[styles.tabContainer, (ifSelected('Other') ? styles.selectedTab : '')]}>
           <TouchableOpacity
             onPress={() => navigate('Other')}
             style={styles.containerBottomItem}>
             <View style={styles.button}>
               <Text>Other</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={[styles.tabContainer, (ifSelected('Total') ? styles.selectedTab : '')]}>
+          <TouchableOpacity
+            onPress={() => navigate('Total')}
+            style={styles.containerBottomItem}>
+            <View style={styles.button}>
+              <Text>Total</Text>
             </View>
           </TouchableOpacity>
         </View>
@@ -69,6 +86,7 @@ const TransactionsStack = createBottomTabNavigator(
     Weekly: {screen: Transactions},
     Monthly: {screen: Transactions},
     Other: {screen: Transactions},
+    Total: {screen: Transactions},
   },
   {
     initialRouteName: 'Daily',
@@ -91,11 +109,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   tabContainer: {
-    width: '25%',
+    justifyContent: 'center',
+    width: '20%',
+    height: '100%'
+  },
+  selectedTab:{
+    color: 'red',
+    // backgroundColor: 'red',
+    borderBottomWidth: 5,
+    borderBottomColor: 'red'
   },
   button: {
     alignSelf: 'center',
-    borderWidth: 2
+    // borderWidth: 2
   }
 });
 export default TransactionsStack;
