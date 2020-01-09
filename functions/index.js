e0d8c9d3a0d96c51b01fed7b435de60222ba463a
davidcher86 <davidcher86@gmail.com>
2020-01-09 07:50:36 +0200

    - h

--- functions/index.js
+++ functions/index.js
@@ -14,22 +14,21 @@ exports.updateNewTransaction = functions.database
     const after = change.after.val();
     console.log('change', change);
 
-    // admin.initializeApp({
-    //   credential: admin.credential.applicationDefault()
-    // });
     var lastUpdate = new Date();
     var newItem = null;
+    var uid = null;
+    console.log(context);
     for (var item in after) {
       // check if the property/key is defined in the object itself, not in parent
       if (!before.hasOwnProperty(item)) {
+        uid = item;
         newItem = after[item];
-        newItem._id = item;
       }
     }
-
-    if (newItem !== null) {
+    console.log('uid:', uid);
+    if (newItem !== null && uid !== null) {
       var dbCreditRef = admin.database().ref(`/users/${context.params.uId}/account/credit`);
-      dbCreditRef.push(newItem)
+      dbCreditRef.update({[uid]: newItem})
       .then(res => {
         return res;
       });
@@ -41,66 +40,29 @@ exports.updateNewTransaction = functions.database
       });
     }
     return null;
-    // var data = await dbRef.once('value', (snapshot) => {
-    //     var event = snapshot.val();
-    //     console.log('snapshot ', snapshot);
-    //     console.log('snapshot ', snapshot.val());
-    //       return snapshot.val();
-    //    })
-    //    return data;
-    // console.log('functions is triggered');
-    // console.log('after', before);
-    // console.log('context', context);
-    // console.log(Object.keys(after).length);
-    // if (Object.keys(after).length === Object.keys(before).length) {
-    //   return null;
-    // }
-    // const timeDate = Date.now();
-    // return change.after.ref.update({ timeDate });
-
-    // return change.after.ref.set({ timeDate });
-    // return null;
-    // return change.after.ref.update({ timeDate });
-    // Only edit data when it is first created.
-    // if (before.before.exists()) {
-    //   return null;
-    // }
-
-    // // Exit when the data is deleted.
-    // if (!change.after.exists()) {
-    //   console.log('sdfsdf', change);
-    //   return null;
-    // }
-
-    // Grab the current value of what was written to the Realtime Database.
-    // const original = change.after.val();
-    // console.log('Uppercasing', context.params.pushId, original);
-    // const uppercase = original.toUpperCase();
-
-    // You must return a Promise when performing asynchronous tasks inside a Functions such as
-    // writing to the Firebase Realtime Database.
-    // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-    // return change.after.ref.parent.child('uppercase').set(uppercase);
   });
 
   exports.deleteTransaction = functions.database
-  .ref('/users/{uId}/account/transactions')
+  .ref('/users/{uId}/account/transactions/{transactionUID}')
   .onDelete(async (change, context) => {
     const before = change.before.val();
     const after = change.after.val();
-    console.log('change', change);
+    console.log('change on delete', change);
 
     var lastUpdate = new Date();
     var newItem = null;
+    var uid = null;
     for (var item in before) {
       // check if the property/key is defined in the object itself, not in parent
       if (!after.hasOwnProperty(item)) {
+        uid = item;
         newItem = after[item];
         newItem._id = item;
       }
     }
     console.log(newItem);
-    if (newItem !== null) {
+    if (newItem !== null && uid !== null) {
+      admin.database().ref(`/users/${context.params.uId}/account/credit/${uid}`);
       // var dbCreditRef = admin.database().ref(`/users/${context.params.uId}/account/credit`);
       // dbCreditRef.push(newItem)
       // .then(res => {
