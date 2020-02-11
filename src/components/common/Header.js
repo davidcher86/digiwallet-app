7e0e70125376495ee1afd0a9d7f633211487d0da
davidcher86 <davidcher86@gmail.com>
2020-02-11 23:07:42 +0200

    - try

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,44 +1,32 @@
 import React, {Component} from 'react';
 import {Header, Icon} from 'react-native-elements';
-import { DARK_MODE } from './../Styles';
+import {DARK_MODE} from './../Styles';
 
-// const MyHeader = props => {
-class MyHeader extends Component {
-  render() {
-    const HamburgerMenu = props => {
-      return (
-        <Icon
-          color="#fff"
-          name="menu"
-          onPress={() => props.navigation.dispatch(props.navigation.actions.openDrawer())}
-        />
-      );
-    };
-    console.log(this.props);
+const MyHeader = props => {
+  console.log(this.props);
+  const HamburgerMenu = props => {
     return (
-      <Header
-        leftComponent={<HamburgerMenu navigation={props.navigation} />}
-        backgroundColor={DARK_MODE.COLORS.HEADER_COLOR}
-        containerStyle={{height: 60, paddingBottom: 25}}
-        headerTitle={this.props.navigation}
-        centerComponent={{
-          text: props.title,
-          style: {color: '#fff', fontWeight: 'bold'},
-        }}
-        // barStyle="dark-content"
-        statusBarProps={{barStyle: 'dark-content'}}
+      <Icon
+        color="#fff"
+        name="menu"
+        onPress={() => props.navigation.dispatch(props.navigation.actions.openDrawer())}
       />
     );
-    }
-};
+  };
 
-const mapStateToProps = state => {
-  return {
-      profile: state.profile,
-      identity: state.identity,
-   };
+  return (
+    <Header
+      leftComponent={<HamburgerMenu navigation={props.navigation} />}
+      backgroundColor={DARK_MODE.COLORS.HEADER_COLOR}
+      containerStyle={{height: 60, paddingBottom: 25}}
+      centerComponent={{
+        text: props.title,
+        style: {color: '#fff', fontWeight: 'bold'},
+      }}
+      // barStyle="dark-content"
+      statusBarProps={{barStyle: 'dark-content'}}
+    />
+  );
 };
-
-export default connect(mapStateToProps, actions)(MyHeader);
 
 export default MyHeader;
